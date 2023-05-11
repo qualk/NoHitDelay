b88330f4418af9e6cd628db29e436b7846c80f2c
qualk <68494669+qualk@users.noreply.github.com>
2023-05-11 14:59:15 +1000

    make it a coremod and actually work lmao

--- src/main/java/xyz/qualk/nohitdelay/NoHitDelay.java
+++ src/main/java/xyz/qualk/nohitdelay/NoHitDelay.java
@@ -4,8 +4,10 @@ import xyz.qualk.nohitdelay.command.ExampleCommand;
 import xyz.qualk.nohitdelay.config.TestConfig;
 import cc.polyfrost.oneconfig.events.event.InitializationEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import cc.polyfrost.oneconfig.utils.commands.CommandManager;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 
 /**
  * The entrypoint of the Example Mod that initializes it.
@@ -14,7 +16,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
  * @see InitializationEvent
  */
 @Mod(modid = NoHitDelay.MODID, name = NoHitDelay.NAME, version = NoHitDelay.VERSION)
-public class NoHitDelay {
+public class NoHitDelay implements preInitializationEvent {
     public static final String MODID = "@ID@";
     public static final String NAME = "@NAME@";
     public static final String VERSION = "@VER@";
@@ -22,11 +24,20 @@ public class NoHitDelay {
     @Mod.Instance(MODID)
     public static NoHitDelay INSTANCE; // Adds the instance of the mod, so we can access other variables.
     public static TestConfig config;
+}
+
+public class NoHitDelay implements IFMLLoadingPlugin {
+    public NoHitDelay() {
+        Mixinbootstrap.init();
+        Mixins.addConfiguration("mixins.nohitdelay.json");
+        MixinEnvironment.getDefaultEnvironment().setSide(MixinEnvironment.Side.CLIENT);
+    }
+}
 
     // Register the config and commands.
     @Mod.EventHandler
-    public void onInit(FMLInitializationEvent event) {
+    public void preInit(FMLInitializationEvent event) {
         config = new TestConfig();
         CommandManager.INSTANCE.registerCommand(new ExampleCommand());
     }
-}
+
